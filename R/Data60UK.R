c393930e1e1767f0fc6444c7333fb93b342f3722
Claudia Vitolo <cvitolodev@gmail.com>
2016-09-06 10:04:41 +0000

    version 0.3.0

---
+++ R/Data60UK.R
@@ -0,0 +1,165 @@
+#' Interface for the Data60UK database catalogue
+#'
+#' @author Claudia Vitolo
+#'
+#' @description This function interfaces the Data60UK database catalogue (available from http://www.nwl.ac.uk/ih/nrfa/pub/index.html) containing 61 datasets. Dataset catalogue is available from http://www.nwl.ac.uk/ih/nrfa/pub/data.html.
+#'
+#' @param bbox bounding box, a list made of 4 elements: minimum longitude (lonMin), minimum latitude (latMin), maximum longitude (lonMax), maximum latitude (latMax)
+#' @param columnName name of the column to filter
+#' @param columnValue value to look for in the column named columnName
+#'
+#' @return This function returns a data frame made of 5 columns: "id" (hydrometric reference number), "name", "location", "Latitude", "Longitude" and "area".
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   # Retrieve the whole catalogue
+#'   catalogueData60UK()
+#'
+#'   # Define a bounding box
+#'   bbox <- list(lonMin=-3.82,latMin=52.41,lonMax=-3.63,latMax=52.52)
+#'
+#'   # Filter the catalogue
+#'   catalogueData60UK(bbox)
+#'   catalogueData60UK(columnName="id",columnValue="62001")
+#' }
+#'
+
+catalogueData60UK <- function(bbox=NULL, columnName=NULL, columnValue=NULL){
+
+  # require(XML)
+  # require(RCurl)
+  # require(rnrfa)
+
+  CatalogueData60UK <- NULL # to avoid note in check
+
+  # Latitude is the Y axis, longitude is the X axis.
+  if (!is.null(bbox)){
+    lonMin <- bbox$lonMin
+    lonMax <- bbox$lonMax
+    latMin <- bbox$latMin
+    latMax <- bbox$latMax
+  }else{
+    lonMin <- -180
+    lonMax <- +180
+    latMin <- -90
+    latMax <- +90
+  }
+
+  # Check the url
+  theurl <- "http://nrfaapps.ceh.ac.uk/datauk60/data.html"
+
+  if( url.exists(theurl) ) {
+
+    message("Retrieving data from live web data source.")
+
+    tables <- readHTMLTable(theurl)
+    n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
+    myTable <- tables[[which.max(n.rows)]]
+    names(myTable) <- c("id", "River", "Location")
+    IDs <- unlist(as.numeric(as.character(myTable$id)))
+
+    # Find grid reference browsing the NRFA catalogue
+    temp <- rnrfa::catalogue(columnName = "id", columnValue = IDs)
+    refs <- temp$gridReference
+    myTable$gridReference <- unlist(refs)
+
+    myTable$Latitude <- temp$lat
+    myTable$Longitude <- temp$lon
+
+    myTable <- subset(myTable, (myTable$Latitude <= latMax & myTable$Latitude >= latMin & myTable$Longitude <= lonMax & myTable$Longitude >= lonMin) )
+
+    if (!is.null(columnName) & !is.null(columnValue)){
+
+      if (columnName == "id" | columnName == "name" | columnName == "location"){
+        myTable <- myTable[which(myTable[,columnName] == columnValue),]
+      }else{
+        message("columnName can only be one of the following: id, name, location")
+      }
+
+    }
+
+    row.names(myTable) <- NULL
+
+    return(myTable)
+
+  }else{
+
+    message("The connection with the live web data source failed.")
+
+  }
+
+}
+
+#' Interface for the Data60UK database of Daily Time Series
+#'
+#' @author Claudia Vitolo
+#'
+#' @description This function extract the dataset containing daily rainfall and streamflow discharge at one of the Data60UK locations.
+#'
+#' @param hydroRefNumber hydrometric reference number
+#' @param plotOption boolean to define whether to plot the results. By default this is set to TRUE.
+#' @param timeExtent is a vector of dates and times for which the data should be retrieved
+#'
+#' @return The function returns a data frame containing 2 time series (as zoo objects): "P" (precipitation) and "Q" (discharge).
+#'
+#' @export
+#'
+#' @examples
+#' # tsData60UK(39015)
+#'
+
+tsData60UK <- function(hydroRefNumber,
+                            plotOption=FALSE,
+                            timeExtent = NULL){
+
+  # require(zoo)
+  # require(XML)
+  # require(RCurl)
+
+  theurl <- paste("http://nrfaapps.ceh.ac.uk/datauk60/data/rq",
+                  hydroRefNumber,".txt",sep="")
+
+  if( url.exists(theurl) ) {
+
+    message("Retrieving data from live web data source.")
+    temp <- read.table(theurl)
+    names(temp) <- c("P","Q","DayNumber","Year","nStations")
+
+    # Combine the first four columns into a character vector
+    date_info <- with(temp, paste(Year, DayNumber))
+    # Parse that character vector
+    datetime <- strptime(date_info, "%Y %j")
+    P <- zoo(temp$P,order.by=datetime) # measured in mm
+    Q <- zoo(temp$Q,order.by=datetime) # measured in m3/s
+
+    myTS <- merge(P,Q)
+
+    if ( is.null(timeExtent) ){
+
+      timeExtent <- seq(as.Date("1980-01-01"), as.Date("1990-12-31"), by="days")
+
+    }
+
+    myTS <- window(myTS,
+                   start=as.POSIXct(head(timeExtent, n=1)[1]),
+                   end=as.POSIXct(tail(timeExtent, n=1)[1]))
+
+    if (plotOption == TRUE){
+
+      temp <- catalogueData60UK(columnName="id",columnValue=hydroRefNumber)
+      stationName <- as.character(temp$name)
+      plot(myTS, main=stationName, xlab="",ylab=c("P [mm/d]","Q [m3/s]"))
+
+    }
+
+    return(myTS)
+
+  }else{
+
+    message("The connection with the live web data source failed.")
+
+  }
+
+}
--- R/TRMM.R
+++ R/TRMM.R
@@ -1,126 +1,175 @@
 #' Download and convert TRMM data
-#' 
+#'
 #' @author Claudia Vitolo
-#' 
-#' @description The TRMM dataset provide global historical rainfall estimation in a gridded format.  
-#' 
-#' @param fileLocation file path where to save the GeoTiff
-#' @param url url where data is stored (e.g. "ftp://disc2.nascom.nasa.gov/data/TRMM/Gridded/3B43_V7/2012/")
+#'
+#' @description The TRMM dataset provide global historical rainfall estimation in a gridded format.
+#'
+#' @param inputLocation location where data is stored. By default it points to the TRMM ftp server ("ftp://disc2.nascom.nasa.gov/data/TRMM/Gridded/") but it can also be a local directory. If you are using a local directory, this function expects to find inside 'inputLocation' a folder with the name of product and version (e.g. "i3B43_V7") and inside this a folder for each year( e.g. "2012").
 #' @param product this is the code that identifies a product, default is "3B43"
 #' @param version this is the version number, default is 7
-#' @param year year of interest, default is 2012
 #' @param type this is the type of information needed, default is "precipitation.accum". Other types could be "gaugeRelativeWeighting.bin" and "relativeError.bin"
-#' @param bbox bounding box, a list made of 4 elements: minimum longitude (lonMin), minimum latitude (latMin), maximum longitude (lonMax), maximum latitude (latMax) 
 #' @param timeExtent is a vector of dates and times for which the data should be retrieve
-#' 
-#' @return Data is loaded as rasterbrick, then converted to a multilayer Geotiff that can 
+#' @param bbox OPTIONAL bounding box, a list made of 4 elements: minimum longitude (lonMin), minimum latitude (latMin), maximum longitude (lonMax), maximum latitude (latMax)
+#' @param outputfileLocation file path where to save the GeoTiff
+#'
+#' @return Data is loaded as rasterbrick, then converted to a multilayer Geotiff that can
 # be opened in any GIS software.
-#' 
-#' @details This code is based upon Martin Brandt's blog post: 
+#'
+#' @details This code is based upon Martin Brandt's blog post:
 #' http://matinbrandt.wordpress.com/2013/09/04/automatically-downloading-and-processing-trmm-rainfall-data/
 #' and on the TRMM FAQ: http://disc.sci.gsfc.nasa.gov/additional/faq/precipitation_faq.shtml
-#' 
+#'
 #' @export
-#' 
-#' @examples 
-#' # Define a bounding box
-#' # bbox <- list(lonMin=-3.82,latMin=52.41,lonMax=-3.63,latMax=52.52)
-#' 
-#' # TRMM(product="3B43",
-#' #      version=7,
-#' #      year=2012,
-#' #      bbox)
+#'
+#' @examples
+#' \dontrun{
+#'   # Define a bounding box
+#'   bbox <- list(lonMin=-3.82, latMin=48,lonMax=-3.63, latMax=50)
+#'   twindow <- seq(as.Date("2012-01-01"), as.Date("2012-01-31"), by="months")
+#'
+#'   TRMM(inputLocation="ftp://disc2.nascom.nasa.gov/data/TRMM/Gridded/",
+#'        product = "3B43",
+#'        version = 7,
+#'        type = "precipitation.accum",
+#'        timeExtent = twindow,
+#'        bbox = bbox,
+#'        outputfileLocation = "~/")
+#'
+#'   # or simply
+#'   TRMM(timeExtent = twindow, bbox = bbox)#'
+#'   plot(brick("~/trmm_acc.tif"))
+#' }
 #'
 
-TRMM <- function(fileLocation = "~/",
-                 url = "ftp://disc2.nascom.nasa.gov/data/TRMM/Gridded/",
+TRMM <- function(inputLocation="ftp://disc2.nascom.nasa.gov/data/TRMM/Gridded/",
                  product = "3B43",
                  version = 7,
-                 year = 2012,
                  type = "precipitation.accum",
+                 timeExtent = NULL,
                  bbox = NULL,
-                 timeExtent = NULL
+                 outputfileLocation = NULL
                  ){
-  
-  # require(raster)
-  # require(rgdal)
-  # require(RCurl)
-  
-  if (!is.null(bbox)){
-    lonMin <- bbox$lonMin
-    lonMax <- bbox$lonMax
-    latMin <- bbox$latMin
-    latMax <- bbox$latMax
-  }else{    
-    lonMin <- -180
-    lonMax <- +180
-    latMin <- -90
-    latMax <- +90
-  }
-  
-  setwd(fileLocation)
-  
-  myURL <- paste(url, product, "_V", version, "/", year, "/", sep="")
-  # by default this is 
-  # myURL <- "ftp://disc2.nascom.nasa.gov/data/TRMM/Gridded/3B43_V7/2012/"
-  
-  filenames <- getURL(myURL, ftp.use.epsv = FALSE, ftplistonly=TRUE, crlf=TRUE)
-  
-  filePaths <- paste(myURL, strsplit(filenames, "\r*\n")[[1]], sep="")
-  
-  # the following line allows to download only files with a certain pattern,
-  # e.g. only certain months or days. 
-  # "*precipitation.accum" means monthly accumulated rainfall here.
-  selectedfilePaths <- filePaths[grep(filePaths, 
-                                      pattern=paste("*",type,sep=""))] 
-  
-  # download files
-  mapply(download.file, selectedfilePaths, basename(selectedfilePaths)) 
-  
-  # Now I create a virtual file as the downloaded TRMM data come as binaries. 
-  # The VRT-file contains all the downloaded binary files with the appropriate 
-  # geo-informations. 
-  # To automate the process, there is a template script in inst/trmm.sh that 
-  # generates the VRT-file (TRMM.vrt) for all 2012 data. 
-  # Change “3B43.12″ accordingly to the downloaded data. 
 
-  fileConn <- file(paste(fileLocation,"myTRMM.sh",sep=""))
-  shOut <- readLines(system.file("trmm.sh", package="hddtools"),-1)
-  shOut[3] <- paste(" <SRS>WGS84</SRS>' >",fileLocation,"TRMM.vrt;",sep="")
-  shOut[4] <- paste("for i in 3B43.",substr(as.character(year),3,4),"*",sep="")
-  shOut[11] <- paste(" </VRTRasterBand>' >>",fileLocation,"TRMM.vrt",sep="")
-  shOut[13] <- paste("echo '</VRTDataset>' >>",fileLocation,"TRMM.vrt",sep="")
-  writeLines(shOut, fileConn)
-  close(fileConn)
-  
-  # Within R, the script (trmm.sh) is executed and the virtual-file (TRMM.vrt)
-  # loaded as a rasterbrick. This is flipped in y direction and the files 
-  # written as multilayer Geotiff. 
-  # This Geotiff contains all the layers for 2012 and can 
-  # be opened in every GIS software.
-  
-  system(paste("sh ",fileLocation,"myTRMM.sh",sep=""))
-  b <- brick(paste(fileLocation,"TRMM.vrt",sep=""))
-  
-  trmm <- flip(b, direction='y')
-  
-  if (!is.null(lonMin) & !is.null(lonMax) & !is.null(latMin) & !is.null(latMax)){
-    # crop to bounding box  
-    e <- extent(latMin, latMax,lonMin, lonMax)
-    trmm <- crop(trmm, e)
-  }  
-  
-  writeRaster(trmm, 
-              filename=paste(fileLocation,"trmm_acc_",year,".tif",sep=""), 
-              format="GTiff", 
-              overwrite=TRUE)
-  
-  message("Removing temporary files")
-  mapply(file.remove, selectedfilePaths, basename(selectedfilePaths)) 
-  file.remove(paste(fileLocation,"TRMM.vrt",sep=""))
-  file.remove(paste(fileLocation,"myTRMM.sh",sep=""))
-  
-  message(paste("Done. The raster-brick was saved in",
-                paste(fileLocation,"trmm_acc_",year,".tif",sep="")))
-  
+  # Check output file location
+  if ( is.null(outputfileLocation) ) outputfileLocation <- getwd()
+  setwd(outputfileLocation)
+
+  # Check bounding box extent is within original raster extent
+  if (is.null(bbox)){
+    bbox <- list(lonMin=-180,latMin=-50,lonMax=+180,latMax=+50) # TRMM max extent
+  }
+  if (bbox$lonMin < -180 | bbox$lonMin > 180) {
+    bbox$lonMin <- -180
+    message(paste("lonMin of bbox is out of the maximum extent [-180,180],",
+                  "new lonMin of bbox is modified as follows:",
+                  "lonMin =", bbox$lonMin))
+  }
+  if (bbox$lonMax < -180 | bbox$lonMax > 180) {
+    bbox$lonMax <- 180
+    message(paste("lonMax of bbox is out of the maximum extent [-180,180],",
+                  "new lonMax of bbox is modified as follows:",
+                  "lonMax =", bbox$lonMax))
+  }
+  if (bbox$latMin < -50 | bbox$latMin > 50) {
+    bbox$latMin <- -50
+    message(paste("latMin of bbox is out of the maximum extent [-50,50],",
+                  "new latMin of bbox is modified as follows:",
+                  "latMin =", bbox$latMin))
+  }
+  if (bbox$latMax < -50 | bbox$latMax > 50) {
+    bbox$latMax <- 50
+    message(paste("latMax of bbox is out of the maximum extent [-50,50],",
+                  "new latMax of bbox is modified as follows:",
+                  "latMax =", bbox$latMax))
+  }
+  bbSP <- bboxSpatialPolygon(bbox)
+
+  # Check time extent
+  years <- unique(format(timeExtent,"%Y"))
+  months <- format(timeExtent,"%m")
+
+  if ( any(years=="NULL",months=="NULL") ){
+
+    message("This function cannot be executed because your timeExtent is not in the correct format. Please enter a suitable timeExtent, then try again.")
+
+  }else{
+
+    selectedfilePaths <- c()
+
+    for (myYear in years){
+
+      myURL <- paste(inputLocation,
+                     product, "_V", version, "/", myYear, "/", sep="")
+
+      filenames <- getURL(myURL,
+                          ftp.use.epsv = FALSE,
+                          ftplistonly=TRUE,
+                          crlf=TRUE)
+
+      # the following line allows to download only files with a certain pattern,
+      # e.g. only certain months.
+      # "*precipitation.accum" means monthly accumulated rainfall here.
+      for (myMonth in months){
+        filePaths <- paste(myURL,product,".",substr(myYear,3,4),myMonth,"01",
+                           ".",version,".",type,sep="")
+
+        selectedfilePaths <- c(selectedfilePaths,filePaths)
+      }
+
+    }
+
+    # download files
+    # Thanks to Fabien Wagner this also works for windows' users!
+    mapply(download.file, selectedfilePaths,
+           basename(selectedfilePaths), method="wget")
+
+    # Now I create a virtual file as the downloaded TRMM data come as binaries.
+    # The VRT-file contains all the downloaded binary files with the appropriate
+    # geo-informations.
+    # To automate the process, there is a template script in inst/trmm.sh that
+    # generates the VRT-file (TRMM.vrt) for all 2012 data.
+    # Change "3B43.12" according to your timeExtent.
+
+    # fileConn <- file(paste(outputfileLocation,"myTRMM.sh",sep=""))
+    fileConn <- file("myTRMM.sh")
+    shOut <- readLines(system.file("extdata/trmm.sh", package="hddtools"),-1)
+    shOut[4] <- paste("for i in ",product,".*",sep="")
+    writeLines(shOut, fileConn)
+    close(fileConn)
+
+    # Within R, the script (trmm.sh) is executed and the virtual-file (TRMM.vrt)
+    # loaded as a rasterbrick. This is flipped in y direction and the files
+    # written as multilayer Geotiff.
+    # This Geotiff contains all the layers for 2012 and can
+    # be opened in any GIS software.
+
+    if (.Platform$OS.type != "unix"){
+      message("Beware this function was tested on a unix machine. If you are not using a unix machine your results could be wrong. Please report any problem to cvitolodev@gmail.com, thanks!")
+    }
+    system(paste("sh","myTRMM.sh"))
+
+    b <- raster::brick("TRMM.vrt")
+    trmm <- raster::flip(b, direction='y')
+
+    # Crop TRMM raster based on bbox, if necessary
+    if ( raster::extent(bbSP) == raster::extent(trmm) ){
+      message("Using full spatial extent.")
+    }else{
+      message("Cropping raster to bbox extent.")
+      trmm <- raster::crop(trmm, bbSP)
+    }
+
+    raster::writeRaster(trmm,
+                        filename="trmm_acc.tif",
+                        format="GTiff",
+                        overwrite=TRUE)
+
+    message("Removing temporary files")
+    file.remove(c("TRMM.vrt","myTRMM.sh"))
+
+    message(paste("Done. The raster-brick was saved in",
+                  paste(outputfileLocation,"/trmm_acc.tif",sep="")))
+
+  }
+
 }
